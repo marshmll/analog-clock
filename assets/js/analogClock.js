b88c4e5b938b787e9bd49df529f30c756dfe03b7
marshmll <renandasilvaoliveiraandrade@gmail.com>
2021-07-17 14:40:34 -0300

    Files

---
+++ assets/js/analogClock.js
@@ -0,0 +1,22 @@
+const $ = document.querySelector.bind(document);
+const secondHand = $(".second-hand");
+const minsHand = $(".min-hand");
+const hourHand = $(".hour-hand");
+
+function setDate() {
+    const now = new Date();
+    const seconds = now.getSeconds();
+    const secondsDegrees = ((seconds / 60) * 360) + 90;
+    secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+
+    const mins = now.getMinutes();
+    const minsDegrees = ((mins / 60) * 360) + ((seconds / 60) * 6) + 90;
+    minsHand.style.transform = `rotate(${minsDegrees}deg)`;
+
+    const hour = now.getHours();
+    const houDegrees = ((hour / 12) * 360) + ((mins / 60) * 30) + 90;
+    hourHand.style.transform = `rotate(${houDegrees}deg)`;
+}
+
+setInterval(setDate, 1000);
+setDate();
